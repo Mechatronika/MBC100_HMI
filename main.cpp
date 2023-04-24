3b22bd566848010dc64ebb2f3a68f114d586efdb
Krasimir Krustev <krasimir.krustev@yahoo.com>
2023-04-24 16:26:15 +0300

    Added shared memory check at startup - which prevent seccond app execution

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSharedMemory>
 
 
 int main(int argc, char *argv[])
 {
+    QSharedMemory sm("HMI");
+    if(!sm.create(8, QSharedMemory::ReadWrite)) return 0;   
+
     QApplication a(argc, argv);
     MainWindow w;
     w.showFullScreen();
