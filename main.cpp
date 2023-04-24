f2bd0d17de6db17d92c73e2d2f5f5c4e924ce940
Krasimir Krustev <krasimir.krustev@yahoo.com>
2023-04-24 17:02:38 +0300

    Some comments

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 
 int main(int argc, char *argv[])
 {
-    QSharedMemory sm("HMI");
-    if(!sm.create(8, QSharedMemory::ReadWrite)) return 0;   
+    QSharedMemory sm("HMI");					// create shared memory
+    if(!sm.create(8, QSharedMemory::ReadWrite)) return 0;   	// check if shared memory exist exit the application
 
     QApplication a(argc, argv);
     MainWindow w;
